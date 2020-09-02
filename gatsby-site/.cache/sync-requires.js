4d97cd39d80edc6962a5f0bc10f7c8514cd14b85
devanship <devanship96@gmail.com>
2020-09-02 18:08:50 -0400

    deployed?

--- gatsby-site/.cache/sync-requires.js
+++ gatsby-site/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/devanshipatel/Desktop/CodeProjs/webpage/gatsby-site/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/devanshipatel/Desktop/CodeProjs/webpage/gatsby-site/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/devanshipatel/Desktop/CodeProjs/webpage/gatsby-site/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/devanshipatel/Desktop/CodeProjs/webpage/gatsby-site/src/pages/page-2.js")))
